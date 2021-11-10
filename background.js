81c3024276faa0a451c2535f44085ecd15449b59
momaji <moziahandnyelah@hotmail.com>
2021-11-09 22:00:44 -0500

    Fixed 2 bugs
    The two bugs were: Not updating tab number when deleting a tab that wasn't the currently active one, and only showing the number of tabs for the currently active window even when multiple windows are open.

--- background.js
+++ background.js
@@ -1,17 +1,21 @@
 //Event listeners for adding, deleting, changing and switching tabs and windows
 chrome.runtime.onInstalled.addListener(()=>countTabs())
-chrome.tabs.onActivated.addListener(activeInfo => countTabs())
-chrome.tabs.onUpdated.addListener(activeInfo => countTabs())
-chrome.windows.onFocusChanged.addListener(windowID => countTabs())
+chrome.tabs.onActivated.addListener(() => countTabs())
+chrome.tabs.onUpdated.addListener(() => countTabs())
+chrome.tabs.onRemoved.addListener(()=>countTabs())
+chrome.windows.onFocusChanged.addListener(() => countTabs())
 
-//counts current number and sets updates the badge to show
+//counts current number of tabs and sets the badge to show it
 function countTabs(){
-    chrome.tabs.query({currentWindow:true}, function(data){
-        chrome.action.setBadgeText(
-          {
-            text: `${data.length}`,
-          },
-          () => {}
-        );
-    })
-}
+  chrome.tabs.query({currentWindow:true}, function(data){
+    chrome.tabs.query({currentWindow:true, active:true}, function(curTab){
+      chrome.action.setBadgeText(
+        {
+          text: `${data.length}`,
+          tabId: curTab[0].id
+        },
+        ()=>{}
+      );
+    });
+  });
+};
